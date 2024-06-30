09fac5b2bf0f6b8c613f9e479d72774c7810890f
alex <alexhalpin0@gmail.com>
2024-06-30 13:31:43 -0400

    first

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # tweet
+
+tweet from the command line
+
+requires the following environment variables
+
+```
+X_CONSUMER_KEY
+X_CONSUMER_KEY_SECRET
+X_ACCESS_TOKEN
+X_ACCESS_TOKEN_SECRET
+```
+
+oauth1 tokens and secrets obtained via the twitter developer console
+
+## usage
+
+```
+tweet "{TWEET}"
+```
+
+or without quotes:
+
+```
+tweet {TWEET}
+```
---
+++ tweet.py
@@ -0,0 +1,35 @@
+import os, sys, argparse
+import tweepy
+
+
+def main():
+    parser = argparse.ArgumentParser(description="send tweets from the terminal")
+
+    parser.add_argument("tweet", nargs="+", type=str, help="the tweet")
+    args = parser.parse_args()
+
+    X_CONSUMER_KEY = os.environ.get("X_CONSUMER_KEY")
+    X_CONSUMER_KEY_SECRET = os.environ.get("X_CONSUMER_KEY_SECRET")
+    X_ACCESS_TOKEN = os.environ.get("X_ACCESS_TOKEN")
+    X_ACCESS_TOKEN_SECRET = os.environ.get("X_ACCESS_TOKEN_SECRET")
+
+    client = tweepy.Client(
+        access_token=X_ACCESS_TOKEN,
+        access_token_secret=X_ACCESS_TOKEN_SECRET,
+        consumer_key=X_CONSUMER_KEY,
+        consumer_secret=X_CONSUMER_KEY_SECRET,
+    )
+
+    tweet = " ".join(args.tweet)
+
+    try:
+        tweet = client.create_tweet(text=tweet)
+        print("done")
+
+    except Exception as e:
+        print(f"tweepy error: {str(e)}", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
